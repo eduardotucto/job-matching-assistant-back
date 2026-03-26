8f45f3e8268e3e3fce3e02fbe209ef01c703d6d8
Deimos <tuctoeduardo@gmail.com>
2026-03-26 13:27:45 -0500

    feat: add user and resume modules with registration logic

--- src/modules/registry.ts
+++ src/modules/registry.ts
@@ -1,6 +1,6 @@
 import type { AppModule } from './moduleContract'
-import { buildUserModule } from './user/user.module.ts'
-import { buildResumeModule } from './resume/resume.module.ts'
+import { buildUserModule } from './user/userModule.ts'
+import { buildResumeModule } from './resume/resumeModule.ts'
 
 export function getAppModules (): AppModule[] {
   return [buildUserModule(), buildResumeModule()]
