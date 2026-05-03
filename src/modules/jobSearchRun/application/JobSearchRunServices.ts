f037b1cf7bd692f83e0dfa3ec88013ddfc9a560d
Deimos <tuctoeduardo@gmail.com>
2026-05-03 18:34:36 -0500

    feat: simplify CreateJobSearchRunUseCase input structure and update createdAt format

--- src/modules/jobSearchRun/application/JobSearchRunServices.ts
+++ src/modules/jobSearchRun/application/JobSearchRunServices.ts
@@ -1,25 +1,15 @@
 import type {
   JobSearchRunEntity,
-  JobMatch,
-  MissingSkill,
   JobSearchRunRepository
 } from '@jobSearchRun/domain'
 
 class CreateJobSearchRunUseCase {
   constructor (private readonly repo: JobSearchRunRepository) {}
 
-  async execute (input: {
-    userId: string;
-    fullName: string;
-    role: string;
-    experience: string;
-    education: string;
-    jobs: JobMatch[];
-    topMissingSkills: MissingSkill[];
-  }): Promise<JobSearchRunEntity> {
+  async execute (input: Omit<JobSearchRunEntity, '_id'>): Promise<JobSearchRunEntity> {
     return this.repo.create({
       ...input,
-      createdAt: new Date().toISOString(),
+      createdAt: new Date()
     })
   }
 }
